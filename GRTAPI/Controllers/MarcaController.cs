3b7e7735c821bed7078bcefb8d41a9272a5c2477
Willans Vicente  Cuellar Cuellar <willans.cuellar@toyosa.com>
2021-08-04 17:08:55 -0400

    commit inicial

---
+++ GRTAPI/Controllers/MarcaController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GRTAPI.Data;
+using GRTAPI.Models;
+
+namespace GRTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarcaController : ControllerBase
+    {
+        private readonly GRTAPIContext _context;
+
+        public MarcaController(GRTAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Marca
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Marca>>> GetMarca()
+        {
+            return await _context.Marca.ToListAsync();
+        }
+
+        // GET: api/Marca/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Marca>> GetMarca(int id)
+        {
+            var marca = await _context.Marca.FindAsync(id);
+
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            return marca;
+        }
+
+        // PUT: api/Marca/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMarca(int id, Marca marca)
+        {
+            if (id != marca.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(marca).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MarcaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Marca
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Marca>> PostMarca(Marca marca)
+        {
+            _context.Marca.Add(marca);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMarca", new { id = marca.Id }, marca);
+        }
+
+        // DELETE: api/Marca/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Marca>> DeleteMarca(int id)
+        {
+            var marca = await _context.Marca.FindAsync(id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            _context.Marca.Remove(marca);
+            await _context.SaveChangesAsync();
+
+            return marca;
+        }
+
+        private bool MarcaExists(int id)
+        {
+            return _context.Marca.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GRTAPI/Data/GRTAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GRTAPI.Models;
+
+namespace GRTAPI.Data
+{
+    public class GRTAPIContext : DbContext
+    {
+        public GRTAPIContext (DbContextOptions<GRTAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GRTAPI.Models.Marca> Marca { get; set; }
+    }
+}
